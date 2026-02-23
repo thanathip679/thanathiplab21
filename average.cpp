494ab7d3d0e4d931211e9f01080c0a32551cd857
thathanathip679 <thanathip.nan@gmail.com>
2026-02-24 02:27:40 +0700

    average.cpp complete

--- average.cpp
+++ average.cpp
@@ -1,1 +1,19 @@
+#include <iostream>
 
+using namespace std;
+
+int main(int argc, char* argv[]){
+    double sum = 0;
+    double result;
+    if(argc == 1){
+        cout << "Please input numbers to find average.";
+    }else{
+        for(int i = 1; i < argc; i++){
+            sum += atof(argv[i]);
+        }
+        result = sum / (argc-1) ;
+        cout << "---------------------------------" << endl;
+        cout << "Average of " << argc-1 << " numbers = " << result << endl;
+        cout << "---------------------------------";
+    }
+}
